84528ffeab021d1074f1a6832678fce49dabcc03
Tayyab Bilal Maniyar <147629380+TB4Maniyar@users.noreply.github.com>
2024-06-03 10:28:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import KgButton from "../KgButton";
+import Hello from "./Hello";
+import Random from "./Random";
+
+function App() {
+  return (
+    <div>
+      <center>
+        <h1>Hello World..!!!</h1>
+        <p>From ReactJs</p>
+      </center>
+    </div>
+  );
+}
+export default App;
